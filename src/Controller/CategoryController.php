e395490ebb57e79ef9db9ab8e2859a9739f13c54
Hristina <hristinastankova89@gmail.com>
2023-12-05 15:02:44 +0100

    creating routes for Category's list and ids

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Theater;
+use App\Form\CategoryType;
+use App\Form\TheaterType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
-    #[Route('/category', name: 'app_category')]
-    public function index(): Response
+    #[Route('/categories', name: 'list_categories')]
+    public function list(CategoryRepository $categoryRepository): Response
     {
         return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+            'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+    #[Route('/categories/{id<\d+>}', name: 'category_item')]
+    public function item(CategoryRepository $categoryRepository, int $id): Response
+    {
+        $category = $categoryRepository->find($id);
+        if ($category === null) {
+            throw new FileNotFoundException('Not found');
+        }
+        return $this->render('category/item.html.twig', [
+            'category' => $category,
         ]);
     }
 }
