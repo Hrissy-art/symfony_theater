7171583dc20fc244f9b0221667a9d1628801b154
Hristina <hristinastankova89@gmail.com>
2023-12-05 14:49:25 +0100

    displaying show articles - list and by Id

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Entity\User;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
-    #[Route('/article', name: 'app_article')]
-    public function index(): Response
+    #[Route('/articles', name: 'list_articles')]
+    public function index(ArticleRepository $articleRepository): Response
     {
-        return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
+        $articles = $articleRepository->findAll();
+        return $this->render('article/list.html.twig', [
+            'articles' => $articles,
+          'title' => 'Show list'
         ]);
     }
-}
+    #[Route('/articles/{id<\d+>}', name: 'item_article')]
+        public function item(ArticleRepository $articleRepository, $id): response
+        {
+            $article = $articleRepository->find($id);
+            return $this->render('article/item.html.twig', [
+    'article' => $article,
+            ]);
+        }
+    }
