0e4dc60487f1cdaa3beaaf1b4bde3e3688159a92
Hristina <hristinastankova89@gmail.com>
2023-12-05 11:10:45 +0100

    creating new entity Article

---
+++ src/Entity/Article.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name_show = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $createdOn = null;
+
+    #[ORM\Column]
+    private ?bool $visible = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $summaryShow = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $img = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNameShow(): ?string
+    {
+        return $this->name_show;
+    }
+
+    public function setNameShow(string $name_show): static
+    {
+        $this->name_show = $name_show;
+
+        return $this;
+    }
+
+    public function getCreatedOn(): ?\DateTimeInterface
+    {
+        return $this->createdOn;
+    }
+
+    public function setCreatedOn(\DateTimeInterface $createdOn): static
+    {
+        $this->createdOn = $createdOn;
+
+        return $this;
+    }
+
+    public function isVisible(): ?bool
+    {
+        return $this->visible;
+    }
+
+    public function setVisible(bool $visible): static
+    {
+        $this->visible = $visible;
+
+        return $this;
+    }
+
+    public function getSummaryShow(): ?string
+    {
+        return $this->summaryShow;
+    }
+
+    public function setSummaryShow(?string $summaryShow): static
+    {
+        $this->summaryShow = $summaryShow;
+
+        return $this;
+    }
+
+    public function getImg(): ?string
+    {
+        return $this->img;
+    }
+
+    public function setImg(?string $img): static
+    {
+        $this->img = $img;
+
+        return $this;
+    }
+}
