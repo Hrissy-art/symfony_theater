3f0a2961c48fb1aa1d19dc32176cd95125ea32d4
Hristina <hristinastankova89@gmail.com>
2023-12-05 15:52:31 +0100

    update form

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -18,25 +18,27 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nameShow',TextType::class, [
+            ->add('nameShow', TextType::class, [
                 'label' => "Titre de l'article"
             ])
-            ->add('author')
-            ->add('summaryShow' ,TextType::class, [
+            // ->add('author')
+            ->add('summaryShow', TextType::class, [
                 'label' => "Summary"
             ])
             ->add('img', FileType::class, [
                 'label' => 'Image de l\'article',
-                'required' => false]) 
+                'required' => false
+            ])
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'name'
-                            ])
-            // ->add('theaters', EntityType::class, [
-            //     'class' => Theater::class,
-            //     'choice_label' => 'name',
-            //     'multiple'   => true ])
-            
+            ])
+            ->add('theatres', EntityType::class, [
+                'class' => Theater::class,
+                'choice_label' => 'name',
+                'multiple' => true
+            ])
+
             ->add('Save', SubmitType::class, []);
         ;
     }
