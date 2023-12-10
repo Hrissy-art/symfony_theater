93adc6d4d946543ebfd97d0baf2cd1f7e5f8de21
Hristina <hristinastankova89@gmail.com>
2023-12-10 09:44:06 +0100

    customize newsletter form

--- src/Form/NewsletterType.php
+++ src/Form/NewsletterType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Newsletter;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +14,18 @@ class NewsletterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('isSubscribed')
+            ->add('email', EmailType::class)
+            ->add('submit', SubmitType::class, [
+                'label' => 'Inscription'
+            ])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Newsletter::class,
+            'data_class'=> Newsletter::class, 
+            'csrf_protection' => false
         ]);
     }
 }
